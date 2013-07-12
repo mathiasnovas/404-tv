be7fd752dead6eaeaf15da76bd99ca9fdd972cbf
Mathias Novas <mathias@07.no>
2013-07-12 14:48:13 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php header('HTTP/1.0 404 Not Found'); ?>
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>404 tv</title>
+        <link rel="stylesheet" href="/404/style.css">
+        <script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+        <script src="/404/js/marquee.js"></script>
+    </head>
+    <body>
+        <div class="tv">
+            <div class="frame">
+                <div class="second-frame">
+                    <div class="monitor">
+                        <div class="image">
+                            <div class="marquee">Du er fortapt. You're lost. Estás perdido. είσαι χαμένος. 你都将丢失。Vous êtes perdu. 당신이 손실됩니다. вы потеряли. Ikaw ay mawawala. あなたは失われます。</marquee>
+                        </div>
+                    </div>
+                </div>
+                <div class="logo">404 <strong>tv</strong></div>
+            </div>
+            <div class="brand">philips</div>
+        </div>
+        <script>
+        $(document).ready(function () {
+            console.log($('.marquee'));
+            $('.marquee').marquee({
+                speed: 15000,
+                gap: 50,
+                delayBeforeStart: 0,
+                direction: 'left',
+                duplicated: true
+            });
+        });
+        </script>
+    </body>
+</html>
